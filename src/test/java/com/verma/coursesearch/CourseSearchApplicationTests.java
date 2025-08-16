1a32f75503682d3da633b0b4bfba6a5bce6c2584
Vermamayank7176 <mayankverma10219@gmail.com>
2025-08-16 10:15:13 +0530

    Initial commit: create README

---
+++ src/test/java/com/verma/coursesearch/CourseSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verma.coursesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
